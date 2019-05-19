06c9a84bc607bf1457194bd6fc672658bb424a74
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2019-05-19 11:58:25 +0300

    First commit

---
+++ fitness/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using fitness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fitnes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- fitness/Properties/AssemblyInfo.cs
+++ fitness/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("fitnes")]
+[assembly: AssemblyTitle("fitness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("fitnes")]
+[assembly: AssemblyProduct("fitness")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
